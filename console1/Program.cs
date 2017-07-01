2356dee8270744872bd392b4cf0333af8e50242d
maksimmarchyk <maksim-nitka2015@yandex.ru>
2017-07-01 12:04:42 +0300

    условие

---
+++ console1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console1
+{
+    //братик помогимне
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
